d6fefb56e1829e131ae2436b51876f59a7325b8d
KristinaRisimovic <kristina.risimovic@gmail.com>
2024-05-29 20:11:04 +0200

    dodat middleware za korisnicke uloge

--- dogadjajiapi/routes/api.php
+++ dogadjajiapi/routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    
+});
+
+Route::middleware(['auth:sanctum','role:korisnik'])->group(function () {
+     
+});
+Route::middleware(['auth:sanctum','role:admin,korisnik'])->group(function () {
+     
 });
