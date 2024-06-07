de41360beecc0e3d3168ed114bb07bc5cce7342a
KristinaRisimovic <kristina.risimovic@gmail.com>
2024-06-08 01:21:47 +0200

    dodate rute

--- dogadjajifront/src/App.js
+++ dogadjajifront/src/App.js
@@ -1,19 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Pocetna from './Komponente/PocetnaStranica/Pocetna';
 import Login from './Komponente/Sesija/Login';
-import { useState } from 'react';
 import Register from './Komponente/Sesija/Register';
 
 function App() {
-  const[user,setUser]=useState();
-  return (
-    <div className="App">
-        <Register></Register>
-      <Login setUser={setUser}></Login>
-      <Pocetna></Pocetna>
+  const [user, setUser] = useState();
 
-    </div>
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Pocetna />} />
+          <Route path="/login" element={<Login setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
