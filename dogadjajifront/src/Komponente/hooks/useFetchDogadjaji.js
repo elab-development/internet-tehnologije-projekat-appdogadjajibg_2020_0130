a472cb70a45cd6c5d1ddcb27afac96340f89d077
katarinajavorac <katarinajavorac8@gmail.com>
2024-06-09 00:45:13 +0200

    kreirane korisnicki definisane kuke

---
+++ dogadjajifront/src/Komponente/hooks/useFetchDogadjaji.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useFetchDogadjaji = () => {
+  const [dogadjaji, setDogadjaji] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchDogadjaji = async () => {
+      const token = sessionStorage.getItem('token');
+
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/dogadjaji', {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        setDogadjaji(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchDogadjaji();
+  }, []);
+
+  return { dogadjaji, loading, error };
+};
+
+export default useFetchDogadjaji;
